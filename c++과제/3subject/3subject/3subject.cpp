59de97ebf89d26f7a22af9ea8047f76a765dbba4
Hyun Joon Lee <117090276+PPIONN@users.noreply.github.com>
2026-03-18 20:01:45 +0900

    내배캠 c++ 3번 과제

---
+++ c++과제/3subject/3subject/3subject.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include "inventory.h"
+using namespace std;
+
+
+
+void main()
+{
+	Inventory<Item>* itemInventory = new Inventory<Item>();
+	for (int i = 0; i < 11; ++i)
+	{
+		itemInventory->AddItem(Item("Item" + to_string(i), i * 100));
+	}
+
+	itemInventory->PrintAllItems();
+
+	delete itemInventory;
+
+
+}
+///
+//- `T* pItems_`: 아이템 객체들을 저장할 * *동적 배열을 가리키는 포인터 * *입니다.
+//- `new T[]`를 통해 메모리를 할당받습니다.
+//- `int capacity_`: 인벤토리가 최대로 저장할 수 있는 공간의 크기입니다.
+//- `int size_`: 현재 인벤토리에 저장된 아이템의 실제 개수입니다.
---
+++ c++과제/3subject/3subject/inventory.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+class Item
+{
+public:
+	Item() {};
+	Item(const string& name, int price) { 
+		name_ = name;
+		price_ = price;
+	}; // 각 멤버 변수를 세팅
+
+	const string& GetName() const { return name_; };
+	int GetPrice() const { return price_; };
+	void Clear()
+	{
+		name_ = "";
+		price_ = 0;
+	}
+
+private:
+	string name_;
+	int price_;
+};
+
+template<typename T>
+class Inventory {
+public:
+	Inventory(int capacity = 10) {
+		if (capacity == 0) { // 예외처리. 입력되지 않는다면 기본적으로 10의 값을 가진다.
+			setcapacity(10);
+		}
+		else {
+			setcapacity(capacity); 
+		}
+		pitems_ = new T[getcapacity()]; // get을 활용해 capacity_의 값만큼 힙메모리 생성.
+
+	}
+	~Inventory() {
+		delete[] pitems_;
+		pitems_ = nullptr;
+	};
+	void setcapacity(int capacity) { // cpapcity_의 set 함수, 최소 1의 값을 가진다.
+		if (capacity < 1) {
+			capacity_ = 1;
+		}
+		else {
+			capacity_ = capacity;
+		}
+
+	}
+	int getcapacity() const { // capacity get함수
+		return capacity_;
+	}
+	int getsize() const {
+		return size_;
+	}
+	void AddItem(const T& item) {
+		if (getsize() == getcapacity()) {
+			cout << "인벤토리가 꽉 찼습니다!" << endl;
+		}
+		else {
+			pitems_[getsize()] = item;
+			++size_;
+		}
+	}
+	void RemoveLastItem() {
+		if (size_ > 0) --size_;
+	}
+	void PrintAllItems() const {
+		for (int i = 0; i < getsize(); i++) {
+			cout << "[이름: " << pitems_[i].GetName() << ", 가격: " << pitems_[i].GetPrice() << "G]" << endl;
+		}
+	}
+	
+
+private:
+	T* pitems_;
+	int capacity_ =0;
+	int size_ = 0;
+	bool check = true;
+
+
+};
